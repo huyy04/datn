fa1077e3e415b898bba21cc2cf2ee383596127cc
Ngoc Dung <seehafer@gmail.com>
2024-10-09 02:44:59 +0700

    Chuyển đổi định dạng dòng

--- DATN/fontend_datn/src/App.js
+++ DATN/fontend_datn/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BrandList from "./components/admin/Brand/BrandList"; // thương hiệu
 import ProductVariantList from "./components/admin/ProductVariant/ProductVariantList"; // biến thể sản phẩm
 import CategoryList from "./components/admin/Category/CategoryList"; // danh mục
@@ -12,18 +12,16 @@ import ProfilePage from "./pages/admin/ProfilePage"; // thông tin cá nhân
 const App = () => {
   return (
     <Router>
-      <div>
-        <Switch>
-          <Route path="/brands" component={BrandList} />
-          <Route path="/product-variants" component={ProductVariantList} />
-          <Route path="/categories" component={CategoryList} />
-          <Route path="/profile" component={Profile} />
-          <Route path="/products" component={ProductListPage} />
-          <Route path="/product/:id" component={ProductDetailPage} />
-          <Route path="/categories/:id" component={CategoryPage} />
-          <Route path="/user/profile" component={ProfilePage} />
-        </Switch>
-      </div>
+      <Routes>
+        <Route path="/brands" element={<BrandList />} />
+        <Route path="/product-variants" element={<ProductVariantList />} />
+        <Route path="/categories" element={<CategoryList />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/products" element={<ProductListPage />} />
+        <Route path="/product/:id" element={<ProductDetailPage />} />
+        <Route path="/categories/:id" element={<CategoryPage />} />
+        <Route path="/user/profile" element={<ProfilePage />} />
+      </Routes>
     </Router>
   );
 };
