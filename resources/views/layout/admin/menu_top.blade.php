bf3b0573f9c9587109e70c396b215c24a7a92c0a
nangnh075 <huynang075.com>
2024-10-23 19:21:58 +0700

    Resolve merge conflicts after pull

--- resources/views/layout/admin/menu_top.blade.php
+++ resources/views/layout/admin/menu_top.blade.php
@@ -394,19 +394,17 @@
                             </a>
                         </li>
                         <li>
-                            <a href="login.html" class="user-item">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
-                                                                     document.getElementById('logout-form').submit();">
-                                    <div class="body-title-2">{{ __('Logout') }}</div>
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                <div class="body-title-2">{{ __('Logout') }}</div>
                             </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
                         </li>
                     </ul>
                 </div>
             </div>
         </div>
     </div>
+</div>
